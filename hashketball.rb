3a9491560d00cc6cee3e68e6a6e801f87e5b59a2
Yuliya Yurkevich <juliana.ny2008@gmail.com>
2020-05-28 17:59:30 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -182,8 +182,9 @@ def player_stats(player_name)
   game_hash.each do |key, value|
     value[:players].each do |ind|
       if ind[:player_name] == player_name 
-        :player_name.to_s
-        ind.delete(:player_name)
+        #:player_name.to_s
+        #ind.delete(:player_name)
+        ind.delete(:name)
         stats=ind
 end
 end
